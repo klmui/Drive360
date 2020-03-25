bd45f266a7f0e98725cc98bb322b132bb1a550fb
Kenny Mui <klmui@wisc.edu>
2020-03-25 02:24:02 -0500

    Create menu and inflate in main activity

--- NativeAndroidApp/app/src/main/java/com/example/drive360/MainActivity.java
+++ NativeAndroidApp/app/src/main/java/com/example/drive360/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -18,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
     public void btnLoadUnity(View v) {
         Intent intent = new Intent(this, MainUnityActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
 //    @Override
