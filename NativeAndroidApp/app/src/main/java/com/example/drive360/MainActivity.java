bb117f2381ae5130122589859d70c1cc4d6f978d
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-03-25 23:48:47 -0500

    add 360vid, add check authentication and redirect to login screen

--- NativeAndroidApp/app/src/main/java/com/example/drive360/MainActivity.java
+++ NativeAndroidApp/app/src/main/java/com/example/drive360/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.drive360;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,7 +16,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        checkAuthentication();
+    }
+
+    // Check if user is authenticate/login.
+    private void checkAuthentication () {
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.drive360", Context.MODE_PRIVATE);
+
+        boolean isAuthenticated = sharedPreferences.getBoolean("isAuthenticated", false);
+        String username = sharedPreferences.getString("username", "");
+        if (isAuthenticated && username != null && !username.equals("")) {
+            setContentView(R.layout.activity_main);
+        } else {
+            goToLoginScreen();
+        }
+    }
+
+    // Redirect the user to login first.
+    public void goToLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     public void btnLoadUnity(View v) {
