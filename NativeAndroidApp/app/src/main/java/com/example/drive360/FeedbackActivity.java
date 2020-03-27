55b080dcccf6b4360e0d3856e74214cf8ed4637b
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-03-27 00:54:58 -0500

    set up spinner/dropdown

--- NativeAndroidApp/app/src/main/java/com/example/drive360/FeedbackActivity.java
+++ NativeAndroidApp/app/src/main/java/com/example/drive360/FeedbackActivity.java
@@ -3,12 +3,53 @@ package com.example.drive360;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class FeedbackActivity extends AppCompatActivity {
+public class FeedbackActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spinner;
+    private static final String[] paths
+            = {"Please choose feedback category", "Report error", "General feedback", "Suggestions", "Others"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        setupSpinner();
+    }
+
+    public void setupSpinner () {
+        spinner = findViewById(R.id.feedbackCategory);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        switch (position) {
+            case 0:
+                Toast.makeText(this, "Please select a feedback category!", Toast.LENGTH_LONG).show();
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 }
