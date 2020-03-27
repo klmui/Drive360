d655fdc2b5f2e43bc8684c7b1ff61241bd1df660
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-03-27 18:24:53 -0500

    configurations to write the first data to firebase

--- NativeAndroidApp/app/src/main/java/com/example/drive360/Feedback.java
+++ NativeAndroidApp/app/src/main/java/com/example/drive360/Feedback.java
@@ -1,15 +1,30 @@
 package com.example.drive360;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Feedback {
     private String username;
+    private String category;
     private String message;
-    private int rating;
+    private float rating;
 
     public Feedback() {}
 
-    public Feedback(String username, String message, int rating) {
+    public Feedback(String username, String category, String message, float rating) {
         this.username = username;
+        this.category = category;
         this.message = message;
         this.rating = rating;
+    }
+
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("username", username);
+        result.put("category", category);
+        result.put("message", message);
+        result.put("rating", rating);
+
+        return result;
     }
 }
--- NativeAndroidApp/app/src/main/java/com/example/drive360/FeedbackActivity.java
+++ NativeAndroidApp/app/src/main/java/com/example/drive360/FeedbackActivity.java
@@ -1,34 +1,59 @@
 package com.example.drive360;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class FeedbackActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class FeedbackActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, ValueEventListener {
+    private FirebaseDatabase firebaseDB;
+    private DatabaseReference rootRef;
+    private DatabaseReference feedbackRef;
     private Spinner spinner;
-    private static final String[] paths
+    private static final String[] feedbackTypes
             = {"Please choose feedback category", "Report error", "General feedback", "Suggestions", "Others"};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
+        // Initialize database, root and feedback references.
+        firebaseDB = FirebaseDatabase.getInstance();
+        rootRef = firebaseDB.getReference();
+        feedbackRef = rootRef.child("feedbacks");
+
         setupSpinner();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     public void setupSpinner () {
         spinner = findViewById(R.id.feedbackCategory);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item,paths);
+                android.R.layout.simple_spinner_item, feedbackTypes);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -54,8 +79,41 @@ public class FeedbackActivity extends AppCompatActivity implements AdapterView.O
     }
 
     public void submitFeedback(View v) {
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.drive360", Context.MODE_PRIVATE);
+
+        // Get current user's username.
+        String username = sharedPreferences.getString("username", "");
+
+        // Get feedback category from spinner/dropdown.
+        Spinner spinner = findViewById(R.id.feedbackCategory);
+        String category = spinner.getSelectedItem().toString();
+
+        // Get message from text field.
+        EditText messageInput = findViewById(R.id.feedbackMessage);
+        String message = messageInput.getText().toString();
+
+        // Get rating from stars rating bar.
         RatingBar ratingBar = findViewById(R.id.feedbackRating);
-        int stars = ratingBar.getNumStars();
         float rating = ratingBar.getRating();
+
+//        String key = firebaseDB.child("feedbacks").push().getKey();
+//        Feedback feedback = new Feedback(username, category, message, rating);
+//        Map<String, Object> feedbackValues = feedback.toMap();
+
+//        Map<String, Object> childUpdates = new HashMap<>();
+//        childUpdates.put("/feedbacks/" + key, feedbackValues);
+        Log.e("username for firebase", username);
+        DatabaseReference fbRef = feedbackRef.child("abc");
+        fbRef.setValue(username);
+    }
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError databaseError) {
+
     }
 }
