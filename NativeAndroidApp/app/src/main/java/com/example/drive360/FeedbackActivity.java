c165fc61cddb805abc6aa6628646a917073ca5c2
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-03-27 00:29:51 -0500

    create layout for feedback screen

---
+++ NativeAndroidApp/app/src/main/java/com/example/drive360/FeedbackActivity.java
@@ -0,0 +1,14 @@
+package com.example.drive360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
