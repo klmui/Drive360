179f880129aca155077d611b1f3cce71058fe22c
Kenny Mui <klmui@wisc.edu>
2020-03-28 23:02:30 -0500

    Load questions dynamically and show current score

--- UnityProject/Assets/Scripts/QuizManager.cs
+++ UnityProject/Assets/Scripts/QuizManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,13 +11,10 @@ public class QuizManager : MonoBehaviour
     // Question where the canvas appears
     public GameObject questionCanvas;
 
-    // Question title
     public Text questionTitle;
 
-    // Score text
     public Text scoreText;
 
-    // Video player
     public VideoPlayer player;
 
     // Flag to indicate whether we are showing questions
@@ -27,14 +25,21 @@ public class QuizManager : MonoBehaviour
     // Time (used to show questions)
     float elapsedTime = 0;
 
+    Question nextQuestion;
+
+    int nextQuestionIndex;
+
+    int totalCorrect = 0;
+
+    int questionsShown = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Video paused
-        player.Pause();
+        scoreText.text = "Score: 0 / 0";
 
-        // No showing questions
-        isShowingQuestions = false;
+        // Pause quiz and don't show question canvas
+        pauseQuiz();
 
         // Hide question canvas
         questionCanvas.SetActive(false);
@@ -57,9 +62,9 @@ public class QuizManager : MonoBehaviour
         quiz.questions[1].title = "Is this correct?";
         quiz.questions[1].correct = true;
 
-        // Initiate showing questions process and play video
-        player.Play();
-        isShowingQuestions = true;
+
+        // Prepare next question
+        prepareNext();
     }
 
     // Update is called once per frame
@@ -72,15 +77,91 @@ public class QuizManager : MonoBehaviour
         elapsedTime += Time.deltaTime;
 
         // Check time, if a question is due, show it
-        if (elapsedTime > quiz.questions[0].time)
+        if (elapsedTime > nextQuestion.time)
         {
             // Show question
 
-            // Show question canvas
+            // 1) Show question canvas
             questionCanvas.SetActive(true);
 
-            // Set question title
-            questionTitle.text = quiz.questions[0].title;
+            // 2) Set question title
+            questionTitle.text = nextQuestion.title;
+
+            // 3) Pause the quiz
+            pauseQuiz();
         }
     }
+
+    void pauseQuiz()
+    {
+        // Video paused
+        player.Pause();
+
+        // No showing questions
+        isShowingQuestions = false;
+    }
+
+    void resumeQuiz()
+    {
+        // Continue playing video
+        player.Play();
+
+        // Continue measuring elapsed time
+        isShowingQuestions = true;
+    }
+
+    private void prepareNext()
+    {
+        if (nextQuestion == null)
+        {
+            nextQuestionIndex = 0;
+            nextQuestion = quiz.questions[nextQuestionIndex];
+        } else
+        {
+            nextQuestionIndex++;
+
+            // Check that there are more questions left
+            if (nextQuestionIndex < quiz.questions.Length)
+            {
+                nextQuestion = quiz.questions[nextQuestionIndex];
+            } else
+            {
+                // No more questions left
+                print("Completed quiz");
+                questionCanvas.SetActive(false);
+                scoreText.text += "\nYou completed the quiz!";
+                player.Play();
+                return;
+            }
+        }
+
+        resumeQuiz();
+    }
+
+    /**
+     * Method to handle an answer. Needs to be public so a button can access it
+     * Occurs every time the user selects a response
+     */
+    public void handleAnswer(bool response)
+    {
+        print("responded : " + response + " the correct answer: " + nextQuestion.correct.ToString());
+        questionsShown++;
+        questionCanvas.SetActive(false);
+
+        // Check if the answer was correct
+        if (response == nextQuestion.correct)
+        {
+            totalCorrect++;
+            scoreText.text = "Correct!";
+        } else
+        {
+            scoreText.text = "Wrong answer!";
+        }
+
+        scoreText.text += "\nScore: " + totalCorrect + " / " + questionsShown;
+
+        // Move on to next question
+        prepareNext();
+    }
+
 }
