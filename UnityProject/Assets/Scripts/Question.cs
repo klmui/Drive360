d8568add93e5feddf23d99899cfc8d5d3de62f25
Kenny Mui <klmui@wisc.edu>
2020-03-28 21:13:09 -0500

    Add Question class

---
+++ UnityProject/Assets/Scripts/Question.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question
+{
+    // Time in seconds
+    public int time;
+
+    // Title of the question / test of the question
+    public string title;
+
+    // Correct answer
+    public bool correct;
+}
