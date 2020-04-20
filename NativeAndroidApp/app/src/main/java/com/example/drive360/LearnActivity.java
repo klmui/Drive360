27a508aecebe81dd4eba3523847b0caea8adb1ea
blcarpenter <blcarpenter@wisc.edu>
2020-04-20 18:53:44 -0500

    Added menu functionality

--- NativeAndroidApp/app/src/main/java/com/example/drive360/LearnActivity.java
+++ NativeAndroidApp/app/src/main/java/com/example/drive360/LearnActivity.java
@@ -1,13 +1,19 @@
 package com.example.drive360;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -269,5 +275,59 @@ public class LearnActivity extends AppCompatActivity implements AdapterView.OnIt
         goToUrl(url);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.drive360", Context.MODE_PRIVATE);
+
+        // Get username and set text of menu item to welcome user.
+        String username = sharedPreferences.getString("username", "");
+        if (username != null && !username.equals("")) {
+            MenuItem item = menu.findItem(R.id.welcome);
+            item.setTitle("Welcome " + username);
+        }
+
+        boolean isAdmin = sharedPreferences.getBoolean("isAdmin", false);
+        if (!isAdmin) {
+            menu.findItem(R.id.admin_dashboard).setVisible(false);
+        }
+        //menu.findItem(R.id.add).setVisible(true);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    public void goToLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    //public void goToMainScreen() {
+    //    Intent intent = new Intent(this, MainActivity.class);
+    //    startActivity(intent);
+    //}
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            SharedPreferences sharedPreferences = getSharedPreferences("com.example.drive360", Context.MODE_PRIVATE);
+
+            // Set isAuthenticated to false and remove username form sharedPreferences.
+            sharedPreferences.edit().putBoolean("isAuthenticated", false).apply();
+            sharedPreferences.edit().remove("username").apply();
+
+            // Redirect the user to login screen.
+            goToLoginScreen();
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
