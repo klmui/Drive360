3bb58bd8229818237488da1acefd9374ae552124
Kenneth Mui <klmui@wisc.edu>
2020-04-25 23:32:23 -0500

    Finish implementing interactive 360 car tour

---
+++ UnityProject/Assets/Scripts/VRCalloutController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+public class VRCalloutController : MonoBehaviour
+{
+    // Canvas that will be shown or hiddent
+    public Canvas canvasToDisplay;
+
+    public bool hoverActivated = true;
+
+    public bool isClickActivated = false;
+
+    // VR interactive item component
+    VRInteractiveItem vrInteractive;
+
+    void Awake()
+    {
+        // White interactive circle
+        vrInteractive = GetComponent<VRInteractiveItem>();
+        canvasToDisplay.enabled = false;
+    }
+
+    void OnEnable()
+    {
+        // Hook on to hovering events
+        if (hoverActivated)
+        {
+            vrInteractive.OnOver += ShowCanvas;
+            vrInteractive.OnOut += HideCanvas;
+        }
+
+        if (isClickActivated)
+        {
+            vrInteractive.OnClick += ToggleCanvas;
+        }
+    }
+
+    void ToggleCanvas()
+    {
+        canvasToDisplay.enabled = !canvasToDisplay.enabled;
+    }
+    
+    void HideCanvas()
+    {
+        canvasToDisplay.enabled = false;
+    }
+
+    void ShowCanvas()
+    {
+        canvasToDisplay.enabled = true;
+    }
+
+    void OnDisable()
+    {
+        // Remote hook to hovering events
+        if (hoverActivated)
+        {
+            vrInteractive.OnOver -= ShowCanvas;
+            vrInteractive.OnOut -= HideCanvas;
+        }
+
+        if (isClickActivated)
+        {
+            vrInteractive.OnClick -= ToggleCanvas;
+        }
+    }
+}
